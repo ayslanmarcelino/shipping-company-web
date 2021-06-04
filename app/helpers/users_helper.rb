89f7cf0425e4954ce05d82756c29315dc246e44b
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-06-04 17:41:12 -0300

    feature/add-cancan Remove useless code
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,11 +4,4 @@ module UsersHelper
               .compact
               .sort
   end
-
-  def roles_collection
-    User::Role.pluck(:kind_cd)
-              .compact
-              .uniq
-              .sort
-  end
 end
