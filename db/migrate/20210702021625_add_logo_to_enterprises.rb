af3874463a600658bc48d87e767bc6de573c6d1a
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-07-02 11:23:17 -0300

    feature/add-carrierwave Fix migration and uploader
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- db/migrate/20210702021625_add_logo_to_enterprises.rb
+++
@@ -1,5 +0,0 @@
-class AddLogoToEnterprises < ActiveRecord::Migration[6.0]
-  def change
-    add_column :enterprises, :logo, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_021625) do
+ActiveRecord::Schema.define(version: 2021_06_03_105222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
 
   create_table "addresses", force: :cascade do |t|
     t.string "city"
@@ -97,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_021625) do
   end
 
   create_table "enterprises", force: :cascade do |t|
+    t.string "logo"
     t.string "primary_color", null: false
     t.string "secondary_color", null: false
     t.string "document_number", null: false
@@ -107,7 +87,6 @@ ActiveRecord::Schema.define(version: 2021_07_02_021625) do
     t.date "opening_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "logo"
     t.index ["document_number"], name: "index_enterprises_on_document_number", unique: true
   end
 
@@ -175,7 +154,6 @@ ActiveRecord::Schema.define(version: 2021_07_02_021625) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "clients", "addresses"
   add_foreign_key "clients", "enterprises"
   add_foreign_key "ctes", "enterprises"
