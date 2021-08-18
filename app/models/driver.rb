fa6c05de4be6886973a8dd6f8634e6c92d2679da
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-08-18 00:40:33 -0300

    feature/fix-project-layouts Refactor layouts and forms
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -47,6 +47,7 @@ class Driver < ApplicationRecord
             if: -> { validate_all }
 
   paginates_per 25
+  cpf_column :document_number
 
   def formatted_name
     "#{person.full_name} | #{person.document_number}"
